03d995a6241b8baea4f654b88c25cfb5f28afad3
Jeymisson Oliveira <jeymissoncz@gmail.com>
2020-06-22 13:53:43 -0300

    Add error test

--- api/date.ts
+++ api/date.ts
@@ -1,6 +1,8 @@
 import { NowRequest, NowResponse } from '@now/node';
 
 export default (_req: NowRequest, res: NowResponse) => {
-  const date = new Date().toString();
-  res.status(200).send(date);
+  const products = {id: 1, name: 'Blouse', description: 'Samll Blouse'};
+  const data = {'products': products};
+  throw Error('Test error!!')
+  res.status(200).send(data);
 };
