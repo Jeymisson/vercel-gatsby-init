6652cabf10b73f254ee4340ffdf67d69c98f266a
Jeymisson Oliveira <jeymissoncz@gmail.com>
2020-06-22 18:24:53 -0300

    Remove error test

--- api/date.ts
+++ api/date.ts
@@ -3,6 +3,5 @@ import { NowRequest, NowResponse } from '@now/node';
 export default (_req: NowRequest, res: NowResponse) => {
   const products = {id: 1, name: 'Blouse', description: 'Samll Blouse'};
   const data = {'products': products};
-  throw Error('Test error!!')
   res.status(200).send(data);
 };
